f2d998114c13da0c8af7062e1eb2aeea0a5abce6
Karin L <karin@daangn.com>
2022-04-12 14:49:43 +0900

    feat: add sg of vpc

---
+++ vpc/sg.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "public" {
+  name = var.name
+
+  description = format("controls access to the ALB for %s", var.name)
+
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = "80"
+    to_port     = "80"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = "443"
+    to_port     = "443"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+}
+
+# output
+
+output "security_group_id" {
+  value = aws_security_group.public.id
+}
